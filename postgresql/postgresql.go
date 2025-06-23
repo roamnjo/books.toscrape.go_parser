bbb9ceb015858882adc568ea6b7d36622805510c
Roma <romch100606@gmail.com>
2025-06-23 23:36:41 +0300

    postgresql commit

---
+++ postgresql/postgresql.go
@@ -0,0 +1,56 @@
+package postgresql
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func Connect() (*Storage, error) {
+	connStr := "postgres://romch:romch@localhost:5432/webparserdb"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Open db error:", err)
+	}
+
+	stmt, err := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS books (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(100) NOT NULL,
+	rating VARCHAR(100) NOT NULL,
+	price VARCHAR(100) NOT NULL,
+	link VARCHAR(100) NOT NULL)
+	`)
+	defer stmt.Close()
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Println(err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveBooks(title, rating, price, link string) (int64, error) {
+	var id int64
+
+	err := s.db.QueryRow(`INSERT INTO books(title, rating, price, link) 
+	VALUES($1, $2, $3, $4) RETURNING id`,
+		title, rating, price, link).Scan(&id)
+
+	if err != nil {
+		return 0, fmt.Errorf("SaveBooks: error:", err)
+	}
+	return id, nil
+}
